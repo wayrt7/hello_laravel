b96ae8bbc1774cbb44772fd4e9366d4cecd74c3e
wayrt7 <wayrt7@gmail.com>
2017-02-20 06:26:10 +0000

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Aufree</div>
+                <div class="title">Hello Laravel! - by Wayrt</div>
             </div>
         </div>
     </body>
